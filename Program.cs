7a27b71be083b6c6a84c417db92d9ef1b94febbd
Nazaka26 <nazar.krava2126@gmail.com>
2020-10-15 14:40:26 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+/*Варіант 3. Профіль студента університету
+    У проекті передбачити розділення інтерфейсу та логіки типів, що створювалися під час виконання завдання. 
+    Передбачити створення виключень під час введення некоректних значень.
+    
+    Дані типу:
+    ·        ПІБ студента: не може бути порожнім рядком або містити цифри;
+    ·        номера залікової книжки: не може містити пробіли;
+    ·        курс: тільки ціле число;
+    ·        середній бал: дійсне число, залежить від оцінок з предметів;
+    ·        оцінки за 10 предметів за 100-бальною шкалою.
+    Методи типу:
+    ·        ініціалізуючий конструктор;
+    ·        встановлення оцінки за індексом предмета;
+    ·        отримання значення оцінки за значенням предмета: всі предмети представляються у перерахуванні з 10 елементів, 
+             у масиві предметів не може бути однакових елементів;
+    ·        перевизначення методу ToString;
+    ·        порівняння 2 студентів за оцінками: у результаті порівняння повертається масив різниці балів у предметах.*/
+namespace SS_task_1
+{
+    public enum Disciplines
+    {
+        English = 0,
+        Mathematicks,
+        History,
+        Geography,
+        Physics,
+        Chemistry,
+        Phylosophy,
+        Literature,
+        Biology,
+        Sociology,
+        Length         //size of enum
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student rick = new Student("Rick Sanchez", "AS2F81S", "2");
+            rick.SetMark(Disciplines.English,92);
+            rick.SetMark(Disciplines.Mathematicks,64);
+            rick.SetMark(Disciplines.History,68);
+            rick.SetMark(Disciplines.Geography,75);
+            rick.SetMark(Disciplines.Physics,82);
+            rick.SetMark(Disciplines.Chemistry,66);
+            rick.SetMark(Disciplines.Phylosophy,85);
+            rick.SetMark(Disciplines.Literature,98);
+            rick.SetMark(Disciplines.Biology,90);
+            rick.SetMark(Disciplines.Sociology,100);
+
+            rick.ViewInfo();
+            Console.WriteLine(rick.ToString());
+            
+            Student morty = new Student("Morty Smith", "DS654S4", "3" );
+            morty.SetMark(Disciplines.English,78);
+            morty.SetMark(Disciplines.Mathematicks,96);
+            morty.SetMark(Disciplines.History,68);
+            morty.SetMark(Disciplines.Geography,88);
+            morty.SetMark(Disciplines.Physics,60);
+            morty.SetMark(Disciplines.Chemistry,97);
+            morty.SetMark(Disciplines.Phylosophy,92);
+            morty.SetMark(Disciplines.Literature,78);
+            morty.SetMark(Disciplines.Biology,85);
+            morty.SetMark(Disciplines.Sociology,90);
+
+            var diff = rick.CompareMarks(morty);
+            Console.WriteLine("The difference between Rick's and Morty's marks: ");
+            foreach (var value in diff)
+                Console.Write((value) + " ");
+
+            Console.WriteLine("\nRick's grade in biology: " + rick.GetMark(Disciplines.Biology));
+            Console.WriteLine("\nRick's grade in biology: " + rick.GetMark(Disciplines.Biology));
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace SS_task_1
+{
+    public class Student
+    {
+        public string _fullName { get; }
+        public string _reportCard { get; }
+        public int _studingYear { get; }
+
+        private int[] _marks = new int[(int)Disciplines.Length]; 
+            // "Disciplines.Length" contains size of the enum.
+            //  We need "marks" array to have the same size as "Disciplines".
+        public float GradePointAverage
+        {
+            get
+            {
+                float res = 0;
+                foreach (var value in _marks)
+                    res += value;
+                return res / (int) Disciplines.Length;
+            }
+        }
+        public Student(string fullName, string reportCard, string studingYear)
+        {
+            if(!NameInputCheck(fullName)) 
+                throw new Exception("Student name can only consist of english words");
+            _fullName = fullName;
+
+            if(string.IsNullOrWhiteSpace(reportCard) || reportCard.Contains(' '))
+                throw new Exception("Report card cannot contain spaces");
+            _reportCard = reportCard.ToUpper();
+
+            if (!Int32.TryParse(studingYear, out int intStudingYear)) 
+                throw new Exception("The year of study must be numeric");
+            _studingYear = intStudingYear;
+        }
+
+        public void ViewInfo()
+        {
+            Console.WriteLine(@"
+Student name:               {0} 
+Student reportCard:         {1}     
+Student year of studying:   {2}     
+Student GPA                 {3}
+", _fullName, _reportCard, _studingYear, GradePointAverage);
+        }
+        public int[] CompareMarks(Student student)
+        {
+            var differences = new int[_marks.Length];
+            for (var i = 0; i < _marks.Length; i++)
+                differences[i] = this._marks[i] - student._marks[i];
+            return differences;
+        }
+       
+        private bool NameInputCheck(string fullName)
+        {
+            if (String.IsNullOrWhiteSpace(fullName))
+                return false;
+            var splitedFullName = fullName.Trim().Split(' ');
+            foreach (var ch in fullName.Replace(" ", ""))
+                if (!((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')))
+                    return false;
+            return true;
+        }
+        
+        public void SetMark(Disciplines disciplines, int mark)
+        {
+            var indexOfDiscipline = (int)disciplines;
+            if (_marks[indexOfDiscipline] != 0)
+                throw new Exception("The mark of the chosen discipline was entered twice");
+            _marks[indexOfDiscipline] = mark;
+        }
+
+        public int GetMark(Disciplines disciplines)
+        {
+            return _marks[(int) disciplines];
+        }
+        public string ToString()
+        {
+            var allMarks = new string[_marks.Length];
+            for (var i = 0; i < _marks.Length; i++)
+                allMarks[i] = (Disciplines)i + ":\t" + _marks[i];
+            return String.Join("\n", allMarks);
+        }
+    }
+}
